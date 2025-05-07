6ca466a54a08d8fc98019789dfc6eb628936c190
Nafa06 <naufalanggardi09@gmail.com>
2025-05-07 15:14:48 +0700

    Menambahkan input panjang data pada fungsi input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -8,7 +8,20 @@ int x;
 
 void input()
 {
-
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyak elemen pada Array:";
+        cin >> nPanjang;
+        if (nPanjang <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
 }
 
 
